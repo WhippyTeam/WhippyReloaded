2a3b9d94c852cfdb0eaf80545d527dc63c00091f
Tymoteusz Boba <Tymoteusz.Boba@gmail.com>
2019-11-30 23:24:56 +0100

    Add fresh Command system. (#1)
    * Add Command API made by TheMolkaPL.
    
    * Remove unnecessary import.
    
    * Replace all errors with a configurable in config string.
    
    * Fix some major bugs, improve locale-en file.
    
    * Refactor code with CodeFactor advices.

---
+++ tools/src/main/java/pl/tymoteuszboba/whippytools/command/system/exception/CommandUsageException.java
@@ -0,0 +1,11 @@
+package pl.tymoteuszboba.whippytools.command.system.exception;
+
+public class CommandUsageException extends CommandException {
+    public CommandUsageException() {
+        super();
+    }
+
+    public CommandUsageException(String message) {
+        super(message);
+    }
+}
