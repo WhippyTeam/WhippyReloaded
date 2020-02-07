bf8869db345863fc46285966b58f7033b37cba1d
Tymoteusz Boba <Tymoteusz.Boba@gmail.com>
2020-02-07 20:03:45 +0100

    Add implementation for interfaces, introduce new exceptions.

---
+++ shared/shared-core/src/main/java/com/whippyteam/shared/bukkit/command/BukkitCommand.java
@@ -0,0 +1,98 @@
+package main.java.com.whippyteam.shared.bukkit.command;
+
+import com.whippyteam.shared.command.Command;
+import java.lang.reflect.Method;
+
+public class BukkitCommand implements Command {
+
+    private final Method method;
+    private final Object commandClassObject;
+
+    private String[] names;
+    private String description;
+    private boolean userOnly;
+    private String[] permissions;
+
+    private int minArgs;
+    private int maxArgs;
+
+    public BukkitCommand(String[] names, String description, int minArgs, int maxArgs, boolean userOnly,
+        String[] permissions, Method method, Object commandClassObject) {
+
+        this.names = names;
+        this.description = description;
+
+        this.minArgs = minArgs;
+        this.maxArgs = maxArgs;
+
+        this.userOnly = userOnly;
+        this.permissions = permissions;
+
+        this.method = method;
+        this.commandClassObject = commandClassObject;
+    }
+
+    public String[] getNames() {
+        return names;
+    }
+
+    public String getPrimaryName() {
+        return names[0];
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public boolean isUserOnly() {
+        return userOnly;
+    }
+
+    public String[] getPermissions() {
+        return permissions;
+    }
+
+    public int getMinArgs() {
+        return minArgs;
+    }
+
+    public int getMaxArgs() {
+        return maxArgs;
+    }
+
+    public Method getCommandMethod() {
+        return method;
+    }
+
+    public Object getCommandObject() {
+        return commandClassObject;
+    }
+
+    public void setNames(String[] names) {
+        this.names = names;
+    }
+
+    public void setPrimaryName(String name) {
+        this.names[0] = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setUserOnly(boolean userOnly) {
+        this.userOnly = userOnly;
+    }
+
+    public void setPermissions(String[] permissions) {
+        this.permissions = permissions;
+    }
+
+    public void setMinArgs(int minArgs) {
+        this.minArgs = minArgs;
+    }
+
+    public void setMaxArgs(int maxArgs) {
+        this.maxArgs = maxArgs;
+    }
+}
---
+++ shared/shared-core/src/main/java/com/whippyteam/shared/bukkit/exception/CommandExecutionException.java
@@ -0,0 +1,5 @@
+package main.java.com.whippyteam.shared.bukkit.exception;
+
+public class CommandExecutionException extends CommandException {
+
+}
